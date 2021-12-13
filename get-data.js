30cfd1328bc9c7db20bec07d0ef54bb5c24736e3
Max Daunarovich <max.daunarovich@gmail.com>
2021-12-14 02:27:51 +0300

    Create account getter

--- get-data.js
+++ get-data.js
@@ -1,28 +1,46 @@
 import fs from "fs";
 import * as fcl from "@onflow/fcl";
-import { setEnvironment } from "flow-cadut";
+import { setEnvironment, extractImports } from "flow-cadut";
+import prettier from "prettier";
+import parserBabel from "prettier/parser-babel";
+
 import addresses from "./addresses";
 
+const filterImports = (imp) => {
+  const keys = Object.keys(imp).filter((item) => !item.includes("/"));
+  const clear = {};
+  for (const key of keys) {
+    clear[key] = imp[key];
+  }
+  return clear;
+};
+
 (async () => {
-  const accountData = [];
+  // const accountData = [];
   await setEnvironment("mainnet");
-  for (const address of addresses.slice(0, 3)) {
-    console.log(`Processing: ${address}`);
-    try {
+  const accountData = [];
+  await Promise.all(
+    addresses.slice(0, 5).map(async (address) => {
+      console.log(`Processing: ${address}`);
       const { account } = await fcl.send([fcl.getAccount(address)]);
+      console.log(`Got: ${address}`);
       const contractNames = Object.keys(account.contracts);
       for (const contractName of contractNames) {
+        const code = account.contracts[contractName];
+        const imports = filterImports(extractImports(code));
+        console.log(contractName);
         accountData.push({
           address,
           contractName,
-          code: account.contracts[contractName],
+          // code,
+          imports,
         });
       }
-    } catch (e) {
-      console.error(e);
-    }
-  }
+    })
+  );
 
-  let data = JSON.stringify(accountData);
+  console.log({ accountData });
+
+  let data = prettier.format(JSON.stringify(accountData), { parser: "json" });
   fs.writeFileSync("deployed-contracts.json", data);
 })();
